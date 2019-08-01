30250233730fd934bcc888a6d6abae7723dda0f8
Vig-git <vigneshrajasekaran96@gmail.com>
2019-08-01 19:34:33 +0530

    new code add for google

---
+++ src/course/CourseDemo.java
@@ -0,0 +1,14 @@
+package course;
+
+public class CourseDemo {
+public void javaCourse() {
+	// TODO Auto-generated method stub
+System.out.println("ganesh");
+}
+	public static void main(String[] args) {
+		CourseDemo js=new CourseDemo();
+				js.javaCourse();
+	}
+	}
+
+
